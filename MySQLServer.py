6dac80848367c3cb13b203f37b12ac097088e6b1
Youssef alaa <youssefaeldin@gmail.com>
2025-07-24 17:53:39 +0300

    dk

---
+++ MySQLServer.py
@@ -0,0 +1,15 @@
+import mysql.connector 
+def connect():
+    try:
+        mydb = mysql.connector.connect(  host="localhost",
+        user="root",
+        password="Asdel1-Asdel1"
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute('CREATE DATEBASE alx_book_store IF NOT EXISTS')
+        print('Database alx_book_store created successfully!')
+    except mysql.connector.Error :
+        print(f'error : {mysql.connector.Error}')
+    finally:
+        mycursor.close()
+        mydb.close()
