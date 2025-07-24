a31890a581d03b9f7375e808a497fb1e89dfbf4a
Youssef alaa <youssefaeldin@gmail.com>
2025-07-24 16:42:49 +0300

    new one done

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,5 @@
-CREATE TABLE Authors(author_id INT PRIMARY KEY, authors_name VARCHAR(215));
+CREATE DATABASE IF NOT EXISTS alx_book_store
+CREATE TABLE Authors(author_id INT PRIMARY KEY, author_name VARCHAR(215) NOT NULL);
 CREATE TABLE Customers(Customer_id INT PRIMARY KEY, customer_name VARCHAR(215) NOT NULL, email VARCHAR(215) UNIQUE, address TEXT NOT NULL);
 CREATE TABLE Books(book_id INT PRIMARY KEY, title VARCHAR(130) NOT NULL, author_id INT, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE NOT NULL);
