6ebf31e4dad84ebc502b45aa8c18a1bece1c0056
Youssef alaa <youssefaeldin@gmail.com>
2025-07-24 18:16:44 +0300

    f

--- task_2.sql
+++ task_2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Authors(author_id INT PRIMARY KEY, authors_name VARCHAR(215));
+CREATE TABLE Authors(author_id INT PRIMARY KEY, author_name VARCHAR(215));
 CREATE TABLE Customers(Customer_id INT PRIMARY KEY, customer_name VARCHAR(215) NOT NULL, email VARCHAR(215) UNIQUE, address TEXT NOT NULL);
 CREATE TABLE Books(book_id INT PRIMARY KEY, title VARCHAR(130) NOT NULL, author_id INT, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE NOT NULL);
