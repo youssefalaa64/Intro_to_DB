21d8be7b7f064ff9bb4a349991f4fbbe2721495e
Youssef alaa <youssefaeldin@gmail.com>
2025-07-24 16:57:10 +0300

    nowww

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,4 +3,4 @@ CREATE TABLE Authors(author_id INT PRIMARY KEY, author_name VARCHAR(215) NOT NUL
 CREATE TABLE Customers(Customer_id INT PRIMARY KEY, customer_name VARCHAR(215) NOT NULL, email VARCHAR(215) UNIQUE, address TEXT NOT NULL);
 CREATE TABLE Books(book_id INT PRIMARY KEY, title VARCHAR(130) NOT NULL, author_id INT, FOREIGN KEY (author_id) REFERENCES Authors(author_id), price DOUBLE NOT NULL, publication_date DATE NOT NULL);
 CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE NOT NULL);
-CREATE TABLE Order_Details(orderdatail_id INT PRIMARY KEY, order_id INT, FOREIGN KEY (order_id) REFERENCES Orders(order_id), customer_id INT, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), quantity DOUBLE NOT NULL);
+CREATE TABLE Order_Details(orderdatail_id INT PRIMARY KEY, order_id INT, FOREIGN KEY (order_id) REFERENCES Orders(order_id), customer_id INT, FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE NOT NULL);
